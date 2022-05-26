5ccc8aef2dce25ecab39c82d91353fa154c82400
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-26 07:14:06 +0200

    Variebles var traning

--- Example.js
+++ Example.js
@@ -1,1 +1,19 @@
-alert("Hello World")
+//alert("Hello World");
+//console.log ("Hello World");
+//console.log(2 + 2);
+
+//function myFunction(){}
+
+//myFunction();
+
+ var myVariables = ("Hello");
+
+console.log (myVariables);
+
+//console.write("hejo")
+ var var1=2;
+ console.log (2);
+var vVar=("Wiktor");
+console.log (vVar);
+var suma = (2+5);
+console.log (suma); 
