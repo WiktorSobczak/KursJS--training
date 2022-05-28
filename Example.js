32f30e5163d72e585087a0995480b7dd692756b3
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-28 12:11:22 +0200

    operators traning

--- Example.js
+++ Example.js
@@ -6,14 +6,23 @@
 
 //myFunction();
 
- var myVariables = ("Hello");
+ //var myVariables = ("Hello");
 
-console.log (myVariables);
+//console.log (myVariables);
 
-//console.write("hejo")
+/*console.write("hejo")
  var var1=2;
  console.log (2);
-var vVar=("Wiktor");
-console.log (vVar);
-var suma = (2+5);
-console.log (suma); 
+ var vVar=("Wiktor");
+ console.log (vVar);
+ var suma = (2+5);
+ console.log (suma); 
+*/
+//Operatory
+var myVar = 5;
+console.log (myVar);//wynik: 5
+console.log (myVar+5);//wynik: 10
+console.log (myVar=myVar /2);//wynik: 2,5 
+console.log (5 + "hello");//wynik:5hello
+console.log (5 + (" ") + "hello");//wynik: 5spacja hello
+console.log ("hello" + " " + "wiktor")//wynik: hellow wiktor
