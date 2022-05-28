1a43f451080ba23cda3ed6bbbe373c3887ce06f6
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-28 12:37:38 +0200

    operators next tranong

--- Example.js
+++ Example.js
@@ -19,10 +19,18 @@
  console.log (suma); 
 */
 //Operatory
-var myVar = 5;
+/*var myVar = 5;
 console.log (myVar);//wynik: 5
 console.log (myVar+5);//wynik: 10
 console.log (myVar=myVar /2);//wynik: 2,5 
 console.log (5 + "hello");//wynik:5hello
 console.log (5 + (" ") + "hello");//wynik: 5spacja hello
 console.log ("hello" + " " + "wiktor")//wynik: hellow wiktor
+*/
+var myVar = 10;//wyrowadzona została zmienna o nazwie myVar ktora jest równa 10
+console.log(myVar=myVar + 5); // wynik: 15 ponieważ wykonana została operacja zwiększania myVar czyli 10 został zwiększony o 5
+console.log(myVar);//wynik:15 ponieważ myVar został zwiększony o 5 i od teraz ma nowa zmienna
+console.log(myVar += 5);//wynik: 20 poniewaz myVar = 15 + 5, dodtakowo ta funkcja jest skrótem od(myVar=myVra = 5)
+console.log(myVar);//wynik: 20 poniewaz parametr (wartość) zmiennej myVar została zmieniona na 20
+console.log(myVar -= 10);// wynik: 10 poniewaz zmienna myVar została amniejszona o 10
+console.log(myVar);//wynik: 10 poniewż znów została zmieniona wartosc funkcji myVar
