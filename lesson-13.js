5cff61ce97cb362ee7bdf491beb7af56bc518d89
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-30 17:12:55 +0200

    lesson 13 and 14

---
+++ lesson-13.js
@@ -0,0 +1,37 @@
+//lesson 13
+/*
+var myAge =5// zmienna o nazwie myAge przechowuje, ma parametr 29(to jest zmienne) 
+if(myAge > 30){//jeśli myAge jest większe od 30 
+    console.log("you are over 30");//jeśli prawda wyświetli się ten napis
+}
+else if(myAge > 20){//jesli myAge jest większy od 20
+    console.log("you are over 20");//jeśli to jest prawda wyświetli się napis z tej linijki
+}
+else if(myAge > 10){//jeśli myAge większy jest od 10
+    console.log("you are over 10");// jeśli prawda wyświetli się napis z tej linjki
+}
+else{//w przeciwnym razie (gdy żaden z powyższych argumentów się nie zgadza)
+    console.log("you are not over 10");//wyświetli się napis z tej linijki
+}
+*/
+//lesson 14
+/*
+var x = 5// zmienna o nazwie x o wartości 5
+console.log(x > 4);//wynik: prawda x jest większy od 4
+console.log(x > 5);//wynik: fałsz x nie jest większy od 5
+console.log(x >=4);//wynik: prawda x jest większy lub równy 4
+console.log(x <= 5);// wynik: prawda x jest mniejszy lub równy 5
+console.log(x == 4);// wynik: fałsz x nie jest równy 4
+console.log(x == 5);// wynik: prawda x jest równy 5
+console.log(x === 5);//wynik: prawda x jest równy 5 i jest tak samo napisany
+console.log(x = "5");// wynik: 5 x zmienia się teraz w napis 5 jest przedstawiony jako cyfra 5
+console.log(x == 5);// wynik: prawda x jest równy 5 
+console.log(x === 5);//wynik; fałsz co prawda x jest równy 5 ale nie jest napisany tak samo 
+console.log(x === "5");// wynik: prawda x jest równy 5 i jest tak samo napisany
+console.log(x = 5);//wynik: x jest równe 5 od teraz do x znów przypisana jest wartośc 5
+console.log(x != 4);//wynik: prawda x jest różne od 5
+console.log(x != 5);//wynik :fałsz x nie jest różne od 5 x to 5
+console.log(x !== 5);//:wynik: fałsz x nie jest różne od 5 x jest równe 5 i jest tak samo zapisane czy jest tego samego typu
+console.log(x = "5");// wynik: od teraz x posiada nowy tym czyli x jest napisem o wartości 5
+console.log(x !== 5);// wynik: prawda x jest różny od "5" wartość ta sama ale rżny typ 
+*/
