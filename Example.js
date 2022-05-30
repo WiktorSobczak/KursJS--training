1ca7ff3b1bdb075dd77385ef1c51f7a183ba3729
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-30 09:41:51 +0200

    Variables lesson 9

--- Example.js
+++ Example.js
@@ -1,25 +1,26 @@
-//alert("Hello World");
-//console.log ("Hello World");
-//console.log(2 + 2);
+//alert("Hello World"); //wynik : pojawi sie po pap przed uruchomieniem strony w przeglądarce
+//console.log ("Hello World"); // wynik: w konsoli pojawi się napis hello world
+//console.log(2 + 2); //wynik: w konsoli pojawi sie wynik dodawania 4
 
-//function myFunction(){}
+/*function myFunction(){} //jest to zapis funkcji w nawiasach wpisujemy parametr (czyli do czego funkcja ma się odnosić) a w klamrach blok kodu (czyli to co funkcja ma zrobić) 
 
-//myFunction();
+myFunction(2); //wywołanie funkcji w consoli
+*/
+ //var myVariables = ("Hello"); // zmienna o nazwie myVariables która przechowuje napis hello
 
- //var myVariables = ("Hello");
+//console.log (myVariables); // wywołanie w konsoli zmiennej o nazwie my Variables
 
-//console.log (myVariables);
-
-/*console.write("hejo")
- var var1=2;
- console.log (2);
- var vVar=("Wiktor");
- console.log (vVar);
- var suma = (2+5);
- console.log (suma); 
+/*document.write("hejo") // wywołanie napisu hejo w przeglądarce na stronie czyli w dokumencie
+ var var1=2; // zmianna o nazwie var1 która przechowuje 2
+ console.log (2);// wywołnie zmiennej w konsoli
+ var vVar=("Wiktor");// zmienna o nazwie var2 przechowująca napis wiktor
+ console.log (vVar);// wywołanie zmiennej o nazwie vVar
+ var suma = (2+5); // zmienna o nazwie suma ktora przechowuje działanie matematyczne 2+5
+ console.log (suma); // wywołanie w konsoli funkcji suma
 */
 //Operatory
-/*var myVar = 5;
+/*film 8
+var myVar = 5;
 console.log (myVar);//wynik: 5
 console.log (myVar+5);//wynik: 10
 console.log (myVar=myVar /2);//wynik: 2,5 
@@ -27,6 +28,8 @@ console.log (5 + "hello");//wynik:5hello
 console.log (5 + (" ") + "hello");//wynik: 5spacja hello
 console.log ("hello" + " " + "wiktor")//wynik: hellow wiktor
 */
+/*film 9
+
 var myVar = 10;//wyrowadzona została zmienna o nazwie myVar ktora jest równa 10
 console.log(myVar=myVar + 5); // wynik: 15 ponieważ wykonana została operacja zwiększania myVar czyli 10 został zwiększony o 5
 console.log(myVar);//wynik:15 ponieważ myVar został zwiększony o 5 i od teraz ma nowa zmienna
@@ -34,3 +37,8 @@ console.log(myVar += 5);//wynik: 20 poniewaz myVar = 15 + 5, dodtakowo ta funkcj
 console.log(myVar);//wynik: 20 poniewaz parametr (wartość) zmiennej myVar została zmieniona na 20
 console.log(myVar -= 10);// wynik: 10 poniewaz zmienna myVar została amniejszona o 10
 console.log(myVar);//wynik: 10 poniewż znów została zmieniona wartosc funkcji myVar
+console.log(++ myVar);//w konsoli wyświetli się 11 (wynik się zapisze)
+console.log(myVar);//wynik: 11 wychodzi to z działania wyżej
+console.log(myVar++);// wynik: 12 ale nie wyświetli sie od razu = 1 będzie w pamięci działa jak myVar=myVar +1 zmienia wartośc zmiennej
+console.log(myVar +3); // wynik 15 dlatego że do 12 zostało dodane 3 
+*/
