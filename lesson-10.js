65609fd51feeac00177caa94e36e88d42c719690
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-30 16:25:11 +0200

    lesson 10-12 (if, else)

---
+++ lesson-10.js
@@ -0,0 +1,61 @@
+/*lesson 10
+var myVar = "Wiktor";// zmnienna o nazwie myVar przechowująca napis wiktor
+document.write(myVar); // wywołanie do dokumentu (wyświetli się na stornie)
+console.log(myVar); // wywołanie do konsoli
+document.write(" " + 10);// wywołanie do dikumentu cyfry 10 znaki przed 10 oznaczają spację
+console.log(50); //wywołanie do konsoli cyfry 50
+console.log(20*5); //wywołanie do konsoli wyniku mnożenia
+document.write(" " + 20/5);// wywołanie do dokumentu wyniku dzielenia
+*/
+//lesson 11 true or false
+/*
+var iLikeEat = true;//zmienna o nawzie iLikeEat która ma wartość true
+console.log(iLikeEat);//wywołanie zmiennej iLikeEat
+console.log(7>5);//wywołanie funkcji true luub false 7 mniejsze od 5
+console.log(7==8); //wywolanie porownania 7 z 8 aby wywołać porównanie używamy podwójnego znaku == można także użyć === wtedy porówna nie tylko wartość ale także zapis
+Boolean(7 > 5); // funkcja służaca do porównywania
+*/
+//lesson 12
+/*var youLikeMeat = true; //zmienna youLikeMeat jest prawdziwa 
+if(youLikeMeat){//jeśli youLikeMeat jest prawdziwe 
+    document.write("here is the meaty menu");//ma wyświetlić się na stronie napis
+}//komunikat wyświetli się
+*/
+/*
+var youLikeMeat = false;// zmienna youLikeMeat jest nieprawdziwa 
+if(youLikeMeat){//jeśli zmienna youLikeMeat jest prawdziwa 
+    document.write("here is the meaty menu");//wywolany zostaje ten komunikat
+}//komunikat nie wyświetli się
+*/
+/*
+var(youLikeMeat) = false; //nie trzaba podawać zmiennej (w tym przykładzie zmienna dotyczy youLikemeat)
+if(7 > 5){// js wie że jest to prawda 
+    document.write("this expression is true");//wyświetli to zdanie 
+}
+*/
+/*
+var youLikeMeat = false;
+var myNum = 10;//zmianna o nazwie mynum która przechowuje 10
+if(myNum > 9){//jeśli funkcja w nawiasie jest prawdziwa 
+    document.write("myNum is greater than 10");//to wyświetli się napis
+}
+*/
+/*
+var youLikeMeat = false;// zmianna youLikeMeat jeśli będzie prz niej  false
+var myNum = 10;
+if(youLikeMeat){
+    document.write("myNum is equal to 10");//jeśli youLikeMeat równe jest true wyświetli się ten napis 
+}
+else{
+    document.write("myNum is not equel 10");//jeąli youLikeMeat równe jest false wyświetli się to
+}
+*/
+/*
+var myNum = 10;// zmienna o nawie myNum przechowuje 10
+if(myNum > 1){//jeśli to co w nawiasie jest true lub false 
+    document.write("MyNum is equel to 10");//jesli prawda to wyświetli sie yen napis
+}
+else{//w przecienym wypadku
+    document.write("MyNum is not equel to 10");//jesli to co w if jest nieprawdą to wyświetli się napis z tej linijki
+}
+*/
