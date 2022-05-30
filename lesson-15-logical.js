bcf722f666102c8b48432e2435e75476c65f9c46
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-05-30 21:59:49 +0200

    lesson 15

---
+++ lesson-15-logical.js
@@ -0,0 +1,28 @@
+//lesson 15
+/*
+var myAge = 27;// zmienna o nazwie myAge która przechowuje wiek
+if(myAge >= 18 && myAge <= 30){//zależność: jeśli zmienna myAge jest większa lub równa 18 i mniejsza lub równa 30 
+    document.write("You can come,  you cool dude");//to na stronie wyświetli sie taki napis
+}
+else{//w przeciwnym wypadku (jeśli nie mieści się w podanym przedziale)
+    document.write("you aint coming");//wyświetli się napis z tej linijki
+}
+*/
+/*
+var myAge = 24;
+if(myAge < 18 || myAge > 30){// jeśli myAge jest mniejsze od 18 lub większy od 30
+    document.write("you cant come");//jeśli prwda to nie możesz przyjść (odwrotność &&)
+}
+else{//w przeciwnym przypadku (czyli jeśli mieści się między 18 i 30)
+    document.write("You can come");// to możesz przyjść
+}
+*/
+/*
+var myAge = 24;
+if(myAge < 18 || myAge >30 || myAge === 25){//jeśli myAge jest mniesze od 18 lub większe od 30 lub równe 25 
+    document.write("you cant come");//jeśli prawda to nie możesz przyjść
+}
+else{
+    document.write("you ca come");//jeśli fałsz to możesz przyjść
+}
+*/
