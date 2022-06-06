65efa0181a60b2a8382a8dc33b3886ee6c25af94
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-06-06 11:21:17 +0200

    break and continue

--- while-for-loops.js
+++ while-for-loops.js
@@ -23,3 +23,26 @@ for(i = 1; i <= links.length; i++){// pętla for zaczyna się od 1, odnosi się 
 }
 document.write("all links now looped");//po wyjściu z petli wyświetli się ta linijka
 */
+
+// lesson 18 BREAK AND CONTINUES
+/*
+for (i = 1; i <= 10; i++){ // Pętla for która zaczyna się do 1. Posiada zbiór elementów mniejszych lub równych 10 oraz po każdym obrociee powiększa się o jeden
+    console.log(i);//wywołanie zmiennej i (parametru i)
+    
+    if(i === 7){//jeśli "i" osiągnie siedem  
+    //jeśli użyjemy console.log w tym miejscy wyświetli sie tylko 7        
+        break;//wtedy pętla zostanie złamana przerwie się.Pętla osiagając liczbę 7 skonczy się
+    }
+    //Jeśli użyjemy console.log w tym miejscu to pętla przerwie się na 6 (skonczy się przed 7)
+}
+console.log("i have broken out the loop");//Gdy pętla osiągnie cyfrę 7 skończy się a wtedy wyświetli się ten napis
+
+for(i = 1; i <= 10; i++){//Petla ze zmienną i która zaczyna sie od 1. Zbiór elementów które bierze pod uwagę to wszystkie mniejjsze lub równe 10 i po każdym okrażeniu "i" powiekszane jest o 1
+          // jeśli console.log wyświetlisie w tym miejscu wyświetlą sie wszystkie cyfry z pętli   
+    if(i===5 || i===3){//jeśli i jest równe 5 lub 3 
+         //jeśli console log wystapi w tym miejscu wyświetlą sie tylko 5 and 3
+        continue;//przeskocz (kontynuuj pomijając)   
+}
+console.log(i);//wywołaj zmienna i(console log zawsze musi być po funkcji continue żeby uzyskać chciany efekt)
+}
+*/
