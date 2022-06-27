97e3e599983c96750bb972d20e28f06b09c70a63
Wiktor Sobczak <wiktor.x.sobczak@gmail.com>
2022-06-27 14:51:03 +0200

    added keyword

--- keyword-29.js
+++ keyword-29.js
@@ -43,3 +43,60 @@ console.log(myCar.maxSpeed);
 myCar.drive(50, 3);
 */
 //lessos 30 constructor functions
+/*
+var myCar = {
+
+    maxSpeed: 70,
+    driver: "Sebas",
+    drive: function(speed, time){
+        console.log(speed * time);
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
+    }
+};
+
+var myCar2 = {
+
+    maxSpeed: 70,
+    driver: "Sebas",
+    drive: function(speed, time){
+        console.log(speed * time);
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
+    }
+};
+
+var myCar3 = {
+
+    maxSpeed: 70,
+    driver: "Sebas",
+    drive: function(speed, time){
+        console.log(speed * time);
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
+    }
+};
+// te 3 obiekty możemy zastąpić jedna funkcją ze słowem kluczowym this
+*/
+var Car = function(maxSpeed, driver){// zmienna Car która przechowuje funkcję
+    this.maxSpeed = maxSpeed;//keyWord
+    this.driver = driver;//keyword
+    this.drive = function(speed, time){//keyword
+        console.log(speed * time);
+    };
+    this.logDriver = function(){//keyword
+        console.log("driver name is " + this.driver);
+    };
+}
+var myCar = new Car(70, "Seba Man");//zmienna mycar która przechowuje obiekt new Car
+var myCar1 = new Car(30, "Humpty Dumpty");
+var myCar2 = new Car(110, "James Bond");
+var myCar3 = new Car(05, "Sebol");
+
+myCar.drive(3,25);
+console.log(myCar1.driver);
+myCar2.logDriver();
+console.log(myCar3.maxSpeed);
